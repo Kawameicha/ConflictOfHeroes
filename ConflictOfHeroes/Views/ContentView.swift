c5f1ca5620c8ac7e9b165d30d0673fe594debc32
Christoph Freier <christoph@freier.fr>
2024-12-18 17:35:55 +0100

    Update ContentView

--- ConflictOfHeroes/Views/ContentView.swift
+++ ConflictOfHeroes/Views/ContentView.swift
@@ -15,10 +15,12 @@ struct ContentView: View {
     @State private var removedUnits: [Unit] = []
     @State private var initialCells: [HexagonCell]
     @State private var missionData: MissionData?
+    @State private var round: Int = 1
     @State private var victoryPoints: Int = 0
     @State private var germanCAP: Int = 0
     @State private var sovietCAP: Int = 0
     @State private var leading: UnitArmy = .german
+    @State private var isShowingReserveUnits: Bool = false
 
     init() {
         let mission = loadMissionData(from: "mission1")
@@ -31,70 +33,28 @@ struct ContentView: View {
     var body: some View {
         NavigationSplitView {
             VStack {
-                GroupBox(
-                    label: Text("\(missionData?.gameSetup.name ?? "") - \(missionData?.gameSetup.date ?? "")")
-                ) {
-                    HStack {
-                        Text("Leading: \(leading.rawValue.capitalized)")
-                        Spacer()
-                        Button(action: {
-                            leading = (leading == .german) ? .soviet : .german
-                        }) {
-                            Image(systemName: "arrow.up.arrow.down")
-                        }
-                    }
-
-                    HStack {
-                        Text("Victory Points: ")
-                        Spacer()
-                        Button(action: { victoryPoints -= 1 }) {
-                            Image(systemName: "minus")
-                        }
-                        Text("\(victoryPoints)")
-                        Button(action: { victoryPoints += 1 }) {
-                            Image(systemName: "plus")
-                        }
-                    }
-
-                    HStack {
-                        Text("German CAP: ")
-                        Spacer()
-                        Button(action: { germanCAP -= 1 }) {
-                            Image(systemName: "minus")
-                        }
-                        Text("\(germanCAP)")
-                        Button(action: { germanCAP += 1 }) {
-                            Image(systemName: "plus")
-                        }
-                    }
-
-                    HStack {
-                        Text("Soviet CAP: ")
-                        Spacer()
-                        Button(action: { sovietCAP -= 1 }) {
-                            Image(systemName: "minus")
-                        }
-                        Text("\(sovietCAP)")
-                        Button(action: { sovietCAP += 1 }) {
-                            Image(systemName: "plus")
-                        }
-                    }
-                }.padding()
-
-                Section(header: Text("Reserve Units")) {
-                    List(reserveUnits, id: \.self) { unit in
-                        UnitRow(unit: unit)
-                            .draggable(unit) {
-                                Text(unit.name)
-                                    .foregroundColor(.black)
-                                    .padding(8)
-                                    .background(Capsule().fill(Color.white))
+                VStack {
+                    if let selectedHexagon {
+                        Section(header: Text("Hexagon Units")) {
+                            List(selectedHexagon.units, id: \.self) { unit in
+                                UnitRow(unit: unit)
+                                    .draggable(unit) {
+                                        Text(unit.name)
+                                            .foregroundColor(.black)
+                                            .padding(8)
+                                            .background(Capsule().fill(Color.white))
+                                    }
                             }
+                        }
                     }
+
+                    d10SimulatorView()
+                    d6SimulatorView()
                 }
             }
+//            .toolbar(removing: .sidebarToggle)
             .navigationSplitViewColumnWidth(190)
-        } content: {
+        } detail: {
             if let missionData {
                 HexagonGridView(
                     cells: $initialCells,
@@ -108,27 +68,126 @@ struct ContentView: View {
             } else {
                 Text("Error loading mission data")
             }
-        } detail: {
-            VStack {
-                if let selectedHexagon {
-                    Section(header: Text("Hexagon Units")) {
-                        List(selectedHexagon.units, id: \.self) { unit in
-                            UnitRow(unit: unit)
-                                .draggable(unit) {
-                                    Text(unit.name)
-                                        .foregroundColor(.black)
-                                        .padding(8)
-                                        .background(Capsule().fill(Color.white))
-                                }
+        }
+        .toolbar {
+            ToolbarItem(placement: .automatic) {
+                Button(action: {
+                    isShowingReserveUnits.toggle()
+                }) {
+                    Image(systemName: "plus")
+                }
+            }
+            ToolbarItem(placement: .status) {
+                GroupBox {
+                    HStack {
+                        Text("Round")
+                            .frame(width: 42)
+
+                        Picker("Round", selection: $round) {
+                            ForEach(1...7, id: \.self) { value in
+                                Text("\(value)").tag(value)
+                            }
                         }
+                        .pickerStyle(MenuPickerStyle())
+                        .frame(width: 45)
+
+                        Divider()
+
+                        Text("VPs")
+                            .frame(width: 25)
+
+                        Picker("Victory Points", selection: $victoryPoints) {
+                            ForEach(1...10, id: \.self) { value in
+                                Text("\(value)").tag(value)
+                            }
+                        }
+                        .pickerStyle(MenuPickerStyle())
+                        .frame(width: 45)
+
+                        Button(action: {
+                            leading = (leading == .german) ? .soviet : .german
+                        }) {
+                            ZStack {
+                                switch leading {
+                                case .german:
+                                    CrossShape(widthFactor: 0.3)
+                                        .foregroundColor(.black)
+
+                                    CrossShape(widthFactor: 0.25)
+                                        .foregroundColor(.white)
+
+                                    CrossShape(widthFactor: 0.2)
+                                        .foregroundColor(.black)
+                                case .soviet:
+                                    StarShape()
+                                        .fill(Color.red)
+                                }
+                            }
+                            .aspectRatio(1.0, contentMode: .fit)
+                            .frame(width: 15)
+                        }
+
+                        Divider()
+
+                        Text("CAPs")
+                            .frame(width: 33)
+
+                        Picker("German CAPs", selection: $germanCAP) {
+                            ForEach(0...15, id: \.self) { value in
+                                Text("\(value)").tag(value)
+                            }
+                        }
+                        .pickerStyle(MenuPickerStyle())
+                        .frame(width: 45)
+
+                        ZStack {
+                            CrossShape(widthFactor: 0.3)
+                                .foregroundColor(.black)
+
+                            CrossShape(widthFactor: 0.25)
+                                .foregroundColor(.white)
+
+                            CrossShape(widthFactor: 0.2)
+                                .foregroundColor(.black)
+                        }
+                        .aspectRatio(1.0, contentMode: .fit)
+                        .frame(width: 15)
+
+                        Text("-")
+
+                        Picker("Soviet CAPs", selection: $sovietCAP) {
+                            ForEach(0...15, id: \.self) { value in
+                                Text("\(value)").tag(value)
+                            }
+                        }
+                        .pickerStyle(MenuPickerStyle())
+                        .frame(width: 45)
+
+                        ZStack {
+                            StarShape()
+                                .fill(Color.red)
+                        }
+                        .aspectRatio(1.0, contentMode: .fit)
+                        .frame(width: 15)
                     }
                 }
-
-                d10SimulatorView()
-                d6SimulatorView()
             }
-            .navigationSplitViewColumnWidth(selectedHexagon != nil ? 190 : 0)
         }
+        .popover(isPresented: $isShowingReserveUnits, arrowEdge: .top) {
+            Section(header: Text("Reserve Units")) {
+                List(reserveUnits, id: \.self) { unit in
+                    UnitRow(unit: unit)
+                        .draggable(unit) {
+                            Text(unit.name)
+                                .foregroundColor(.black)
+                                .padding(8)
+                                .background(Capsule().fill(Color.white))
+                        }
+                }
+            }
+            .frame(width: 190, height: 300)
+        }
+        .navigationTitle(Text("\(missionData?.gameSetup.name ?? "") - \(missionData?.gameSetup.date ?? "")"))
         .onAppear {
             gameManager.contentView = self
 
