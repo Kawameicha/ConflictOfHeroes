c915ab386ad3553a3c2dd1c2487d4461eb88a595
Christoph Freier <christoph@freier.fr>
2024-12-31 11:57:59 +0100

    Update GameUnitView

--- ConflictOfHeroes/Views/GameUnitView.swift
+++ ConflictOfHeroes/Views/GameUnitView.swift
@@ -13,9 +13,37 @@ struct GameUnitView: View {
     var body: some View {
         ScrollView {
             LazyVGrid(columns: Array(repeating: .init(.flexible()), count: 2), spacing: 4) {
-                ForEach(units, id: \.id) { unit in
-                    UnitTokenView(unit: unit)
-                        .draggableUnit(unit)
+                let groupedUnits = Dictionary(grouping: units) { $0.identifier }
+
+                let sortedKeys = groupedUnits.keys.sorted { lhs, rhs in
+                    lhs.army.rawValue < rhs.army.rawValue || (lhs.army.rawValue == rhs.army.rawValue && lhs.name < rhs.name)
+                }
+
+                ForEach(sortedKeys, id: \.self) { key in
+                    if let unitsWithGroup = groupedUnits[key] {
+                        ZStack(alignment: .center) {
+                            switch key.name {
+                            case "Control":
+                                ControlTokenView(unit: unitsWithGroup[0])
+                                    .draggableUnit(unitsWithGroup[0])
+                            case "Smoke":
+                                SmokeTokenView(unit: unitsWithGroup[0])
+                                    .draggableUnit(unitsWithGroup[0])
+                            default:
+                                UnitTokenView(unit: unitsWithGroup[0])
+                                    .draggableUnit(unitsWithGroup[0])
+                            }
+
+                            if unitsWithGroup.count > 1 {
+                                HStack {
+                                    Image(systemName: "\(unitsWithGroup.count).circle.fill")
+                                        .symbolRenderingMode(.palette)
+                                        .foregroundStyle(.white, .red)
+                                }
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                            }
+                        }
+                    }
                 }
             }
             .padding(4)
